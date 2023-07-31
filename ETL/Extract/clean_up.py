39a0b0b988e36324a57f493e34e991606817489d
Kwame Marfo <kwame.marfo@fdmgroup.com>
2023-07-31 16:22:19 +0100

    fixed bug in clean_up

--- ETL/Extract/clean_up.py
+++ ETL/Extract/clean_up.py
@@ -2,7 +2,7 @@ import glob
 import os
 from datetime import datetime
 
-class clean_up:
+class Clean_up:
     
     def __init__(self):
         self.regime = 'regime'
@@ -10,25 +10,30 @@ class clean_up:
         self.person_two = 'person_two'
         self.folder_path = 'ETL/Extract/Downloaded_Files/'
     
-    def files(self):
-        # Ensure there are a maximum of 6 files with 2 for each type
-        file_counts = {self.regime: 0, self.person_one: 0, self.person_two: 0}
-        files = glob.glob(f'{self.folder_path}*')
+    def remove_oldest_file(self, files):
         removed_files = []
-
-        for file in files:
-            file_name = os.path.basename(file).split('_')
-            file_name = "_".join(file_name[:-3])
-            if file_name in file_counts:
-                file_counts[file_name] += 1
-                if file_counts[file_name] > 2:
-                    oldest_file = min(files, key=lambda x: datetime.strptime
-                                      ("_".join(os.path.basename(x).split('_')[-3:]).split('.')[0], '%Y_%m_%d'))
-                    os.remove(oldest_file)
-                    removed_files.append(oldest_file)
-
-        if removed_files:
-            removed_file_names = [os.path.basename(file) for file in removed_files]
-            return f"Removed files: {', '.join(removed_file_names)}"
-        else:
-            return "No files were removed"
+        while len(files) > 2:
+            oldest_file = min(files, key=lambda x: datetime.strptime(
+                "_".join(os.path.basename(x).split('_')[-3:]).split('.')[0], '%Y_%m_%d'))
+            os.remove(oldest_file)
+            removed_files.append(oldest_file)
+            files.remove(oldest_file)
+        return removed_files
+            
+    
+    def files(self):
+        file_types = {
+            self.person_one: [],
+            self.person_two: [],
+            self.regime: []
+        }
+        
+        for file_type in file_types:
+            file_pattern = os.path.join(self.folder_path, f'{file_type}*')
+            file_types[file_type] = glob.glob(file_pattern)
+        
+        removed_files = {}
+        for file_type, files in file_types.items():
+            removed_files[file_type] = self.remove_oldest_file(files)
+        
+        return removed_files
